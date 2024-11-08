ca09853b5ffb3b8061747ff22d59318803dc4de3
suren-hov <shovhannisyan2021@gmail.com>
2024-11-08 07:50:05 +0400

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3333;
 const mongoose = require("mongoose");
 
+app.use(cors());
 app.use(express.json());
 mongoose
   .connect("mongodb://localhost:27017/my-referal-tool")
