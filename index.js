f8f213bb96e205c8058b938eb7de020a81ad79b3
Hovhannisyan00 <ars.hovhannisyan00@gmail.com>
2024-11-08 08:11:52 +0400

    commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // Import CORS
 const app = express();
 const PORT = process.env.PORT || 3333;
 const mongoose = require("mongoose");
 
-app.use(cors());
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
+
+// mongoose connection
 mongoose
   .connect("mongodb://localhost:27017/my-referal-tool")
   .then(() => {
@@ -22,33 +24,30 @@ const User = mongoose.model("User", userSchema);
 const str1 = "22987";
 const str2 = "10001";
 
+// POST /users endpoint
 app.post("/users", async (req, res) => {
   const data = req.body; // id = data.userID
-
   if (!data || typeof data.userID !== "number") {
     return res.status(400).json({ error: "Invalid data provided" });
   }
 
   try {
     const idKey = data.userID + str1 + data.userID + str2 + data.userID;
-    console.log(idKey, 9999);
-
     const saved = await new User({ userID: idKey, referralCount: 0 });
     await saved.save();
 
     res.status(201).json({ message: saved });
   } catch (error) {
-    return res.status(500).send("ahahahahahahahah");
+    return res.status(500).send("Server error");
   }
 });
 
+// POST /update/referrals/:id endpoint
 app.post("/update/referrals/:id", async (req, res) => {
   const idKey = req.params.id;
   console.log(idKey);
 
-  res.send(idKey);
   const data = req.body; // id = data.userID
-
   if (!data || !data.userID) {
     return res
       .status(400)
@@ -73,7 +72,8 @@ app.post("/update/referrals/:id", async (req, res) => {
   }
 });
 
-app.get("/conut", async (req, res) => {
+// GET /count endpoint
+app.get("/count", async (req, res) => {
   try {
     const totalReferrals = await User.countDocuments();
     res.json({ totalReferrals });
@@ -83,27 +83,22 @@ app.get("/conut", async (req, res) => {
   }
 });
 
+// GET /user/:id endpoint
 app.get("/user/:id", async (req, res) => {
   const idKey = req.params.id;
-  try{
+  try {
     const user = await User.findOne({ userID: idKey });
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
     res.json(user);
-
-  }catch(e) {
+  } catch (e) {
     console.log("Error in getting user:", e);
     res.status(500).json({ error: e.message });
   }
 });
 
-
-
-
-
-
-
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
